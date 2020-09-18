0334964447c48c2d50d14a8e4a8a2043b4f6a51f
andrey.korchenkov <andrey.korchenkov@rtlabs.ru>
2020-09-18 16:40:39 +0300

    Добавлена зависимость для валидаций.

--- src/main/java/coffeemachine/controller/BuyCoffeeController.java
+++ src/main/java/coffeemachine/controller/BuyCoffeeController.java
@@ -2,10 +2,12 @@ package coffeemachine.controller;
 
 import coffeemachine.dto.BuyCoffeeMessageDto;
 import coffeemachine.dto.CoffeeTypeDto;
+import coffeemachine.exception.NoSuchCoffeeTypeException;
 import coffeemachine.exception.NoSuchCoffeeVariantException;
 import coffeemachine.service.BuyCoffeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 
 @RestController
@@ -21,13 +24,22 @@ public class BuyCoffeeController {
     private final BuyCoffeeService buyCoffeeService;
 
     @PostMapping("/buy")
-    public BuyCoffeeMessageDto buyCoffee(@RequestBody CoffeeTypeDto coffeeTypeDto) {
+    public BuyCoffeeMessageDto buyCoffee(@Valid @RequestBody CoffeeTypeDto coffeeTypeDto) {
         return buyCoffeeService.makeCoffeeIfAvailableAndGetMessage(coffeeTypeDto);
     }
 
-    @ExceptionHandler(NoSuchCoffeeVariantException.class)
+    @ExceptionHandler(NoSuchCoffeeTypeException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public HashMap<String, String> handleIndexOutOfBoundsException(Exception e) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        response.put("error", e.getClass().getSimpleName());
+        return response;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public HashMap<String, String> handleMethodArgumentNotValidException(Exception e) {
         HashMap<String, String> response = new HashMap<>();
         response.put("message", e.getMessage());
         response.put("error", e.getClass().getSimpleName());
--- src/main/java/coffeemachine/dto/CoffeeTypeDto.java
+++ src/main/java/coffeemachine/dto/CoffeeTypeDto.java
@@ -2,10 +2,13 @@ package coffeemachine.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CoffeeTypeDto {
+    @NotBlank(message = "coffee type must not be blank")
     private String coffeeType;
 }
