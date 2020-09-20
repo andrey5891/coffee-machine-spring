3025e9e8f96faeb958adfcb76df0a07f9f2e7ba4
andrey.korchenkov <andrey.korchenkov@rtlabs.ru>
2020-09-20 20:02:32 +0300

    добавление документации к сервисам

--- src/main/java/coffeemachine/service/BuyCoffeeService.java
+++ src/main/java/coffeemachine/service/BuyCoffeeService.java
@@ -3,6 +3,16 @@ package coffeemachine.service;
 import coffeemachine.dto.BuyCoffeeMessageDto;
 import coffeemachine.dto.CoffeeTypeDto;
 
+/**
+ * Service for buying coffee by DTO with string of coffee type
+ * @author Andrey Korchenkov
+ * @version 1.0
+ */
 public interface BuyCoffeeService {
+    /**
+     * Attempt buy coffee by coffee type DTO with string of coffee type
+     * @param coffeeTypeDto - DTO with coffee type
+     * @return DTO with string message about successful purchase
+     */
     BuyCoffeeMessageDto makeCoffeeIfAvailableAndGetMessage(CoffeeTypeDto coffeeTypeDto);
 }
