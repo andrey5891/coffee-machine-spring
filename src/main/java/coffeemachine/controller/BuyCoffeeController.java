9453c640e6edce7fbaafa8bf75a99abff7518f52
andrey.korchenkov <andrey.korchenkov@rtlabs.ru>
2020-09-21 08:55:24 +0300

    рефакторинг

--- src/main/java/coffeemachine/controller/BuyCoffeeController.java
+++ src/main/java/coffeemachine/controller/BuyCoffeeController.java
@@ -2,15 +2,13 @@ package coffeemachine.controller;
 
 import coffeemachine.dto.BuyCoffeeMessageDto;
 import coffeemachine.dto.CoffeeTypeDto;
-import coffeemachine.exception.NoSuchCoffeeTypeException;
 import coffeemachine.service.BuyCoffeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,22 +19,4 @@ public class BuyCoffeeController {
     public BuyCoffeeMessageDto buyCoffee(@Valid @RequestBody CoffeeTypeDto coffeeTypeDto) {
         return buyCoffeeService.makeCoffeeIfAvailableAndGetMessage(coffeeTypeDto);
     }
-
-    /*@ExceptionHandler(NoSuchCoffeeTypeException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public HashMap<String, String> handleIndexOutOfBoundsException(Exception e) {
-        HashMap<String, String> response = new HashMap<>();
-        response.put("message", e.getMessage());
-        response.put("error", e.getClass().getSimpleName());
-        return response;
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public HashMap<String, String> handleMethodArgumentNotValidException(Exception e) {
-        HashMap<String, String> response = new HashMap<>();
-        response.put("message", e.getMessage());
-        response.put("error", e.getClass().getSimpleName());
-        return response;
-    }*/
 }
